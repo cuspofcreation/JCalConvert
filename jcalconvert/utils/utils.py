897daec26c62a05e9207c1c5d7691e9fc8e08c58
Alex Fisher <a.fisher615@gmail.com>
2023-12-18 11:11:24 +0000

    feat: Delineated lookup and convert functions, both now take both JP and Western cal input

--- jcalconvert/utils/utils.py
+++ jcalconvert/utils/utils.py
@@ -6,21 +6,28 @@ import re
 # f = open('./data/calObj.json')
 # j = json.load(f)
 
-# Handles year inputs 
-def yearChecker(year: int):
+# Determines whether input year is a number (Western calendar) or string (Japanese calendar input), 
+# providing relevant logic.
+ 
+def yearChecker(year: int | str):
 
+    if type(year) == int:
     # Handle edge cases
-        if type(year) != int:
-            try:
-                int(year)
-            except:
-                return "Please specify a valid year"
-        
-        elif year > datetime.date.today().year:
+        if year > datetime.date.today().year:
             raise typer.BadParameter("Please specify a valid year")
         
         elif year < 645:
             raise typer.BadParameter("Data only available from 645 CE")
+        
+    elif type(year) == str:
+
+        res = split_string_int(year)
+        
+        if res == "Please specify a valid year":
+            raise typer.BadParameter(res)
+
+    else:
+        raise typer.BadParameter("Please input a valid Western calendar year or Japanese Imperial Calendar year")
 
 # Splits Japanese calendar inputs into an era string and a year string
 def split_string_int(input_string: str):
@@ -31,37 +38,52 @@ def split_string_int(input_string: str):
     if not match:
         match = re.match(r'(\D+)(\d+)', input_string)
     if match:
-        # string_part = match.group(1)
-        # int_part = int(match.group(2))
         return { 'era': match.group(1), 'year': int(int(match.group(2))) }
     else:
         return ("Please specify a valid year")
-        # return None, None  # Return None if the input format doesn't match
 
-# Retrieves the corresponding era object for a given Gregorian calendar year.
-def eraFinder(json, year: int, verbose: bool=False):
+# Retrieves the corresponding era object for a given Western or Japanese Imperial calendar year.
+def eraLookup(json, year: int | str, verbose: bool=False):
 
-    for key, obj in json.items():
-        start_year = float(obj['Start'].split('.')[0])
-        end_year = float(obj['End'].split('.')[0])
+    if type(year) == int:
+
+        for key, obj in json.items():
+            start_year = float(obj['Start'].split('.')[0])
+            end_year = float(obj['End'].split('.')[0])
+
+            if start_year <= year <= end_year:
+                return obj
+            
+    if type(year) == str:
+        input_split = split_string_int(year)
+
+        for key, obj in json.items():
+
+            if input_split['era'] == obj['Japanese'] or obj['Era_no_diacritics']:
+                return obj
+            
+        
+# Take in a date string in the form either, e.g., 平成21 or Heisei 21
+def calConvert(json, year: int | str):
+
+    if type(year) == int:
+
+        for key, obj in json.items():
+            start_year = float(obj['Start'].split('.')[0])
+            end_year = float(obj['End'].split('.')[0])
 
         if start_year <= year <= end_year:
-            if (verbose):
-                return obj
-            else:
                 era_name = obj['Era name']
                 start_year = float(obj['Start'])
                 era_year = int(year - start_year + 2)
                 return(f'{era_name} {era_year}')        
-        
-# take in a date string in the form either, e.g., 平成21 or Heisei 21
-def jLookup(json, input_year: str):
 
-    input_split = split_string_int(input_year)
+    if type(year) == str:
+        input_split = split_string_int(year)
 
-    for key, obj in json.items():
+        for key, obj in json.items():
 
-        if input_split['era'] == obj['Japanese'] or obj['Era_no_diacritics']:
-            start_year = float(obj['Start'])
-            return (int(start_year) + input_split['year'] - 1)
+            if input_split['era'] == obj['Japanese'] or obj['Era_no_diacritics']:
+                start_year = float(obj['Start'])
+                return (int(start_year) + input_split['year'] - 1)
 
