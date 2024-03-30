1663601a82dcb931c92b60b137ebaeb8d61d53c7
Alex Fisher <a.fisher615@gmail.com>
2024-03-30 20:38:06 +0900

    feat: Add unit tests for splitStringInt

--- jcc/tests/test_utils.py
+++ jcc/tests/test_utils.py
@@ -1,10 +1,10 @@
-from jcc.utils.utils import yearChecker
+from jcc.utils.utils import yearChecker, splitStringInt, eraSearch
 import datetime
 import pytest
+import json
+
 
 # Test yearChecker
-
-
 def test_current_year():
     year = str(datetime.date.today().year)
     result = yearChecker(year)
@@ -24,8 +24,39 @@ def test_past_year():
     assert result is None
 
 
-def test_prehistoric_year():
+def test_year_645():
+    year = "645"
+    result = yearChecker(year)
+    assert result is None
+
+
+def test_year_before_645():
     with pytest.raises(Exception) as e_info:
         year = "600"
         yearChecker(year)
     assert str(e_info.value) == "Data only available from 645 CE"
+
+
+# Test splitStringInt
+def test_splitStringInt_valid_input_en():
+    year = "Heisei 21"
+    result = splitStringInt(year)
+    assert result == {"era": "Heisei ", "year": 21}
+
+
+def test_splitStringInt_valid_input_jp():
+    year = "平成21"
+    result = splitStringInt(year)
+    assert result == {"era": "平成", "year": 21}
+
+
+def test_splitStringInt_invalid_input_onlynums():
+    year = "123"
+    result = splitStringInt(year)
+    assert result == "Please specify a valid year"
+
+
+def test_splitStringInt_invalid_input_onlychars():
+    year = "aBc"
+    result = splitStringInt(year)
+    assert result == "Please specify a valid year"
